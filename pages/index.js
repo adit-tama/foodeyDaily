32df02e17708ac73cd8b63a176fb511ac297e005
Praditya Adhitama <pradityadhitama@gmail.com>
2020-05-18 15:28:45 +0700

    revert

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,9 @@ const HomePage = () => {
 	const [keyword, setKeyword] = useState('')
 	return (
 		<>
+			<Head>
+        		<link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;400;700&display=swap" rel="stylesheet" />
+      		</Head>
 			<DesktopLayout
 				section1={<MainSection keyword={keyword} setKeyword={setKeyword}/>}
 				section2={<RecipeSection keyword={keyword} />}
