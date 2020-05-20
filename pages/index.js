984da320f38564307fcd59ba7b821bc0c5669c01
Praditya Adhitama <pradityadhitama@gmail.com>
2020-05-20 15:58:17 +0700

    init

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Test = styled.div`
+	width: 100px;
+	height: 100px;
+	background: red;
+`
+
+
+export default () => (
+  <Test>Welcome to next.js!</Test>
+)
